2a2591a242ce7e67d44e2ded0ff102b4f987adea
Edward Woodcock <edward.woodcock@xbridge.com>
2012-08-28 15:13:08 +0100

    Extracted version number into var. Ensured directory creation.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,10 +1,17 @@
 class redis{
-	file { "/tmp/redis/redis-2.4.16":
+
+	$redis_version = "2.4.16"
+
+	file { "/tmp/redis/":
+	    ensure=>"directory"
+	}
+
+	file { "/tmp/redis/redis-${redis_version}":
 	    ensure=>"directory"
 	}
 
 	exec{'redis-download-install':
-	  command=>"wget --no-check-certificate -r -P /tmp/redis http://redis.googlecode.com/files/redis-2.4.16.tar.gz; tar -xzf /tmp/redis/redis.googlecode.com/files/redis-2.4.16.tar.gz -C /tmp/redis/redis-2.4.16; make -C /tmp/redis/redis-2.4.16; make install -C /tmp/redis/redis-2.4.16",
+	  command=>"sudo wget --no-check-certificate -r -P /tmp/redis http://redis.googlecode.com/files/redis-${redis_version}.tar.gz; sudo tar -xzf /tmp/redis/redis.googlecode.com/files/redis-${redis_version}.tar.gz -C /tmp/redis; sudo make -C /tmp/redis/redis-${redis_version}; sudo make install -C /tmp/redis/redis-${redis_version}",
 	  creates=>'/usr/local/bin/redis-server',
 	  logoutput=>true,
 	  timeout=>1800,
